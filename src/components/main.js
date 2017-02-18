1f11c8824dfcd01b1395752ed7398219872768ef
Bill Wong <b6wong@gmail.com>
2017-02-18 10:20:53 -0500

    Initial commit

---
+++ src/components/main.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    render() {
+        return (
+            <h2>Welcome to Sheriff of Nottingham</h2>
+        );
+    }
+}
+
+export default Main;
+
+
+
+ 
