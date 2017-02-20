2442e8e75c0bb38e9d315caa0256cbc51ea84535
Bill Wong <b6wong@gmail.com>
2017-02-20 16:30:26 -0500

    feat (Players): implement add player logic
    Can add up to five players.

--- src/components/main.js
+++ src/components/main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Modal, ModalHeader, ModalTitle, ModalClose, ModalBody, ModalFooter } from 'react-modal-bootstrap';
 
 class Main extends Component {
 
@@ -7,6 +8,7 @@ class Main extends Component {
         this.state = {
             players: [],
             addPlayer: '',
+            updatePlayer: null
         };
 
         this.handlePlayerChange = this.handlePlayerChange.bind(this);
@@ -34,7 +36,16 @@ class Main extends Component {
         });
     }
 
+    onFinishUpdatePlayer() {
+        console.log("close modal");
+        this.setState({
+            updatePlayer: null
+        })
+    }
+
     render() {
+
+        const canAddMorePlayers = this.state.players.length < 5;
 
         const playerList = this.state.players.map((obj, idx) => {
             return (<li key={idx}>{obj.name}</li>);
@@ -42,11 +53,34 @@ class Main extends Component {
 
         return (
             <div>
-                <h2>Welcome to Sheriff of Nottingham {this.state.players.length}</h2>
-                <input type="text" value={this.state.addPlayer} onChange={this.handlePlayerChange} />
-                <button onClick={this.onAddPlayer} disabled={this.state.addPlayer === ''}>
-                    Add Player
-                </button>
+
+                <Modal isOpen={this.state.updatePlayer !== null}>
+                    <ModalHeader>
+                        <ModalClose onClick={() => this.onFinishUpdatePlayer()} />
+                        <ModalTitle>Update Player</ModalTitle>
+                    </ModalHeader>
+                    <ModalBody>
+                        <p>Update Player: ---</p>
+                    </ModalBody>
+                    <ModalFooter>
+                        <button className="btn btn-default btn-sm" onClick={() => this.onFinishUpdatePlayer()}>
+                            Close
+                        </button>
+                    </ModalFooter>
+                </Modal>
+
+                <h2>Welcome to Sheriff of Nottingham</h2>
+
+                { canAddMorePlayers ?
+                    <div>
+                        <input type="text" value={this.state.addPlayer} onChange={this.handlePlayerChange} />
+                        <button onClick={this.onAddPlayer} disabled={this.state.addPlayer === ''}>
+                            Add Player
+                        </button>
+                    </div> :
+                    null
+                }
+                
                 <ul>
                     {playerList}
                 </ul>
